085e9499793fa379d7af44edaed290993fd165cd
giribabu22 <Giribabu22@navgurukul.org>
2024-01-24 12:28:02 +0530

    added corsOptions

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,30 @@
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
-console.log('BASE_URL: ', process.env.BASE_URL);
+const { PORT, BASE_URL } = process.env;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Enable CORS
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
   const config = new DocumentBuilder()
     .setTitle('NG zuvy API Docs')
-    .setDescription(`[Base url: ${process.env.BASE_URL}]`)
+    .setDescription(`[Base url: ${BASE_URL}]`)
     .setVersion('1.0')
-    // .setBasePath(`${process.env.BASE_URL}`)
-    .addCookieAuth('optional-session-id', { type: 'apiKey', name: 'Authorization',in: 'cookie' })
-    // .setSchemes(['http', 'https'])
-    .build()
+    .addCookieAuth('optional-session-id', { type: 'apiKey', name: 'Authorization', in: 'cookie' })
+    // .addSecurity('basic', { type: 'http', scheme: 'basic' })
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('apis', app, document);
-
-  await app.listen(process.env.PORT || 6000);
+  await app.listen(PORT || 6000);
+  console.log(`Application is running on swagger: ${BASE_URL}/apis#/`);
 }
 bootstrap();
