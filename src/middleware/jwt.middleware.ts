8bd34002ea96ba1d958502779d836f269bba20dc
VaishnaV <aniket.vaishnav@getweave.com>
2025-06-02 00:43:40 +0530

    Fixed type errors

--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -11,6 +11,12 @@ import { eq, sql, count } from 'drizzle-orm';
 import { users, sansaarUserRoles } from '../../drizzle/schema';
 import { helperVariable } from 'src/constants/helper';
 
+function serializeBigInt(obj: any) {
+  return JSON.stringify(obj, (key, value) =>
+    (typeof value) === 'bigint' ? value.toString() : value
+  )
+}
+
 @Injectable()
 export class JwtMiddleware implements NestMiddleware {
   constructor(private readonly jwtService: JwtService) { }
@@ -25,7 +31,8 @@ export class JwtMiddleware implements NestMiddleware {
     ];
 
     const unrestricted = unrestrictedRoutes.some(
-      route => req._parsedUrl.pathname === route.path && req.method === route.method
+      (route) =>
+        req._parsedUrl.pathname === route.path && req.method === route.method,
     );
 
     if (unrestricted) {
@@ -41,22 +48,37 @@ export class JwtMiddleware implements NestMiddleware {
     try {
       const decoded: any = await this.jwtService.decode(token);
       if (!decoded) {
-        throw new UnauthorizedException('Invalid token');
+        throw new UnauthorizedException('Invalid token xx');
       }
 
+      // failing over here
+      console.log(`Decoded token: ${JSON.stringify(decoded)}`);
+      console.log(`id ${decoded.id}`);
+      console.log(`email ${decoded.email}`);
+      // let allUsers = await db.select().from(users).where(eq(users.id, decoded.id));
+      // console.log(`All users: ${serializeBigInt(allUsers)}`);
       const user: any[] = await db
         .select()
         .from(users)
         .where(sql`${users.id} = ${decoded.id} AND ${users.email} = ${decoded.email}`);
+      console.log(`User found: ${serializeBigInt(user)}`);
       if (user.length === 0) {
+        console.log(`User not found for id ${decoded.id} and email ${decoded.email}`);
         throw new UnauthorizedException('User is not authorized');
+      }
+
+      const allRoles = await db.select().from(sansaarUserRoles);
+      for (let _role of allRoles) {
+        console.log('====================================');
+        console.log(_role);
+        console.log('====================================');
       }
 
       const rolesArray = await db
         .select()
         .from(sansaarUserRoles)
         .where(sql`${sansaarUserRoles.userId} = ${user[0].id}`);
-      user[0].roles = rolesArray.map(role => role.role);
+      user[0].roles = rolesArray.map((role) => role.role);
 
       req.user = user;
 
@@ -74,6 +96,7 @@ export class JwtMiddleware implements NestMiddleware {
       if (error instanceof UnauthorizedException || error instanceof ForbiddenException) {
         throw error;
       } else {
+        console.log(`Error: ${error.message}`);
         throw new UnauthorizedException('Invalid token');
       }
     }
