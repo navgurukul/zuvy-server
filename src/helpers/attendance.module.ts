5a448701c1d4f81220834b793a445f4f072d5be7
Prem L <preml@example.com>
2025-06-19 14:21:50 +0530

    Remove Attendance module and service files

--- src/helpers/attendance.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { AttendanceService } from './attendance.service';
-
-@Global()
-@Module({
-  providers: [AttendanceService],
-  exports: [AttendanceService],
-})
-export class AttendanceModule {}
