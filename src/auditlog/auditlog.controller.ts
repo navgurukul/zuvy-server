6ae18aac47e750a6711096f381816d8da6525a3e
Numanazad23 <namanajid54a@gmail.com>
2025-10-09 12:05:05 +0530

    audit comment out

--- src/auditlog/auditlog.controller.ts
+++ src/auditlog/auditlog.controller.ts
@@ -30,42 +30,42 @@ export class AuditlogController {
   })
   @ApiBearerAuth('JWT-auth')
   async createAudit(@Body() createAuditlogDto: CreateAuditlogDto) : Promise<any> {
-    return this.auditlogService.createAudit(createAuditlogDto);
+    return this.auditlogService.log('perm_to_role', createAuditlogDto);
   }
 
-   @Get('/')
-  @ApiOperation({ summary: 'Get all Audit log data' })
-  @ApiQuery({
-    name: 'limit',
-    required: false,
-    type: Number,
-    description: 'Number of bootcamps per page',
-  })
-  @ApiQuery({
-    name: 'offset',
-    required: false,
-    type: Number,
-    description: 'Offset for pagination',
-  })
-  @ApiBearerAuth('JWT-auth')
-  async getAllAudit(
-      @Query('limit') limit,
-      @Query('offset') offset,) {
-    return this.auditlogService.getAllAudit(Number(limit) || 10, Number(offset) || 0);
-  }
+//    @Get('/')
+//   @ApiOperation({ summary: 'Get all Audit log data' })
+//   @ApiQuery({
+//     name: 'limit',
+//     required: false,
+//     type: Number,
+//     description: 'Number of bootcamps per page',
+//   })
+//   @ApiQuery({
+//     name: 'offset',
+//     required: false,
+//     type: Number,
+//     description: 'Offset for pagination',
+//   })
+//   @ApiBearerAuth('JWT-auth')
+//   async getAllAudit(
+//       @Query('limit') limit,
+//       @Query('offset') offset,) {
+//     return this.auditlogService.getAllAudit(Number(limit) || 10, Number(offset) || 0);
+//   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.auditlogService.findOne(+id);
-  }
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.auditlogService.findOne(+id);
+//   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuditlogDto: UpdateAuditlogDto) {
-    return this.auditlogService.update(+id, updateAuditlogDto);
-  }
+//   @Patch(':id')
+//   update(@Param('id') id: string, @Body() updateAuditlogDto: UpdateAuditlogDto) {
+//     return this.auditlogService.update(+id, updateAuditlogDto);
+//   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.auditlogService.remove(+id);
-  }
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.auditlogService.remove(+id);
+//   }
 }
