b6c5e361ebe3eff69b9f55178ff0110a6be16b58
sama <sama@MacBookPro.lan>
2026-06-12 17:24:51 +0530

    worked on the calculation logic in learners profile

--- src/controller/learnerInformation/learner.profile.service.ts
+++ src/controller/learnerInformation/learner.profile.service.ts
@@ -408,18 +408,19 @@ export class LearnerProfileService {
       { key: 'collegeStream', isFilled: !!profile?.collegeStream },
       { key: 'collegeScore', isFilled: !!profile?.collegeScore },
       { key: 'collegeScoreType', isFilled: !!profile?.collegeScoreType },
-      { key: 'class12Board', isFilled: !!profile?.class12Board },
-      { key: 'class12Score', isFilled: !!profile?.class12Score },
-      {
-        key: 'class12ScoreType',
-        isFilled: !!profile?.class12ScoreType,
-      },
-      { key: 'class10Board', isFilled: !!profile?.class10Board },
-      { key: 'class10Score', isFilled: !!profile?.class10Score },
-      {
-        key: 'class10ScoreType',
-        isFilled: !!profile?.class10ScoreType,
-      },
+
+      // { key: 'class12Board', isFilled: !!profile?.class12Board },
+      // { key: 'class12Score', isFilled: !!profile?.class12Score },
+      // {
+      //   key: 'class12ScoreType',
+      //   isFilled: !!profile?.class12ScoreType,
+      // },
+      // { key: 'class10Board', isFilled: !!profile?.class10Board },
+      // { key: 'class10Score', isFilled: !!profile?.class10Score },
+      // {
+      //   key: 'class10ScoreType',
+      //   isFilled: !!profile?.class10ScoreType,
+      // },
       // Only require work experience for experienced users (not for freshers)
       ...(profile?.hasWorkExperience === true
         ? [
@@ -431,13 +432,14 @@ export class LearnerProfileService {
             },
           ]
         : []),
-      {
-        key: 'codingPlatformProfile',
-        isFilled:
-          hasCodingPlatformData(profile?.leetcodeProfiles) ||
-          hasCodingPlatformData(profile?.codechefProfiles) ||
-          hasCodingPlatformData(profile?.codeforcesProfiles),
-      },
+
+      // {
+      //   key: 'codingPlatformProfile',
+      //   isFilled:
+      //     hasCodingPlatformData(profile?.leetcodeProfiles) ||
+      //     hasCodingPlatformData(profile?.codechefProfiles) ||
+      //     hasCodingPlatformData(profile?.codeforcesProfiles),
+      // },
 
       // PAGE 4: PREFERENCES
       {
@@ -453,8 +455,8 @@ export class LearnerProfileService {
           (profile.preferredLocations as any[]).length > 0,
       },
       { key: 'openToRemote', isFilled: profile?.openToRemote === true },
-      { key: 'internshipStipend', isFilled: !!profile?.internshipStipend },
-      { key: 'fullTimeCtc', isFilled: !!profile?.fullTimeCtc },
+      // { key: 'internshipStipend', isFilled: !!profile?.internshipStipend },
+      // { key: 'fullTimeCtc', isFilled: !!profile?.fullTimeCtc },
       {
         key: 'preferredContactMethods',
         isFilled:
